750229b1349fc6dafbcac764963c6d4dda5bd81a
Abhishek Kange <84184526+Abhishekkange@users.noreply.github.com>
2024-11-21 11:57:05 +0530

    binary searhc using recurrsion

---
+++ searching/recursive_binary_search.java
@@ -0,0 +1,45 @@
+package searching;
+
+public class recursive_binary_search {
+
+    static int func(int[] arr,int num,int start,int end)
+    {   
+        if(start>=end)
+        {
+            return -1;
+        }
+
+        //calculate mid
+        int mid = (start+end)/2;
+        if(arr[mid]==num)
+        {
+            return mid;
+        }
+        else{
+
+            if(arr[mid]>num)
+            {
+               return func(arr,num,start,mid-1);
+            }
+            else
+            {
+               return func(arr,num,mid+1,end);
+            }
+    
+
+        }
+       
+
+    }
+    
+
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5,6,8};
+        System.out.println(func(arr, 4,0,arr.length));
+
+        
+
+    }
+}
